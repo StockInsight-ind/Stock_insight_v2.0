2b521b33a35c2e02b89718ee71d66a6acd17c620
Atharva Pathak <atharvapathak@Atharvas-MacBook-Air.local>
2026-06-13 23:04:02 +0530

    Describe what you changed

---
+++ stock-insight-api/src/Controller/UserController.js
@@ -0,0 +1,47 @@
+const userService =
+require('../Service/userService');
+
+const register = async(req,res)=>{
+
+    try{
+
+        const {
+            firstName,
+            lastName,
+            email,
+            password
+        } = req.body;
+
+        if(
+            !firstName ||
+            !lastName ||
+            !email ||
+            !password
+        ){
+
+            return res.status(400).json({
+                message:"All fields required"
+            });
+        }
+
+        const user =
+            await userService.registerUser({
+                firstName,
+                lastName,
+                email,
+                password
+            });
+
+        res.status(201).json(user);
+
+    }catch(error){
+
+        res.status(400).json({
+            message:error.message
+        });
+    }
+};
+
+module.exports = {
+    register
+};
---
+++ stock-insight-api/src/Routes/userroutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+const userController =
+require('../Controller/UserController');
+
+router.post(
+    '/register',
+    userController.register
+);
+
+module.exports = router;

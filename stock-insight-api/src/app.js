3b5be74ee194c50b91264b4ca06e85b5fcf01d76
AjinyaNaik <ajinkya0210@gmail.com>
2026-06-25 18:37:26 -0400

    Removed unneccasry files

--- stock-insight-api/src/app.js
+++ stock-insight-api/src/app.js
@@ -29,7 +29,7 @@ const express = require('express');
 const cors = require('cors');
 
 const userRoutes = require('./Routes/userroutes');
-const marketRoutes = require('./Routes/marketRoutes');
+
 const { initDb } = require('./Repositary/database_communication');
 
 const app = express();
@@ -39,7 +39,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
-app.use('/api/markets', marketRoutes);
 
 app.get('/health', (req, res) => {
     res.status(200).json({ status: 'ok' });

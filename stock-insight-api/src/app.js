a54637ca0119dcc8d75885d39ed7c84779bb651e
AjinyaNaik <ajinkya0210@gmail.com>
2026-06-27 20:40:59 -0400

    Added setup for integration testing

--- stock-insight-api/src/app.js
+++ stock-insight-api/src/app.js
@@ -1,42 +1,19 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 
-const cors = require('cors');
-
-const userRoutes = require('./Routes/userroutes');
-
-const  sequelize  = require('./config/database');
+const userRoutes = require("./Routes/userroutes");
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 
-app.get('/health', (req, res) => {
-    res.status(200).json({ status: 'ok' });
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
 });
 
-const PORT = process.env.PORT || 5050;
-
-const startServer = async () => {
-    try {
-      await sequelize.authenticate();
-     console.log("Database connected.");
-
-        await sequelize.sync();
-
-        console.log("Models synchronized.");
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
-    } catch (error) {
-        console.error('Failed to initialize database:', error);
-        process.exit(1);
-    }
-};
-
-startServer();
+module.exports = app;
